d12ddfb0117ea50f5258e886fdadfabd1c9cdef6
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-05-29 10:38:53 +0530

    Connect mongodb atlas to compass and compass connect tomy node js

--- server.js
+++ server.js
@@ -1,27 +1,50 @@
- //server insances 
-const express = require('express'); 
-//active server on 3000 port
- const app = express();    
+const { MongoClient } = require("mongodb");
+ const express = require('express');
  
-//used to parse req.body in express -> PUT or POST
- const bodyparser = require('body-parser');
 
-//Specifically Parse JSON data and Add it to the request.body object
- app.use(bodyparser.json());
+ const app = express();
+ const bodyParser = require('body-parser');
+ //parse json data and add it to in req body
+ app.use(bodyParser.json());
 
-//active the server
-app.listen(3000, () => {
-    console.log("Server Started at port no. 3000");
+ app.listen(3000, () => {
+  console.log("Server start at port 3000");
+ })
+
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://Root:12345@cluster0.ajizdin.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url);
+async function run() {
+    try {
+        await client.connect();
+        console.log("Connected atles correctly to server");
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+//create routes
+
+app.get('/', (req, res)=>{
+  res.send("Hello Hello");
 });
-//get request
-app.get('/', (req, res) => {
-  res.send("Hello Ji");
-})
-//post request
+
 app.post('/api/cars', (req, res)=>{
   const {name, brand} = req.body;
   console.log(name);
   console.log(brand);
-  res.send("Car data Posted");
-
+  res.send("Car Details Added Successfully !!");
 })
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://Root:12345@cluster0.ajizdin.mongodb.net/myDatabase',{
+  useNewUrlParser:true,//configurations
+  useUnifiedTopology: true,//configurations
+})
+.then(()=> {console.log("Connection Establish nodejs with mongocompas")})
+.catch((error)=> {console.log("Receieved an Error")});
